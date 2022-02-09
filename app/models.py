0ccd14bf1dddc1c3d679c1fe988fc0040299dd0b
Ken Weaver <kbweaver221@gmail.com>
2022-02-08 23:18:44 -0500

    add some tests to further demonstrate behavior

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models.signals import m2m_changed
+from django.db.models.signals import m2m_changed, post_save
 from django.dispatch import receiver
 
 class Related(models.Model):
@@ -21,12 +21,25 @@ class Other(models.Model):
     m2m_field = models.ManyToManyField(to=OtherRelated)
 
 @receiver(m2m_changed, sender=Abstract.m2m_field.through)
-def abstract_signal(sender, **kwargs):
-    wrapped_function(sender, **kwargs)
+def abstract_m2m_signal(sender, **kwargs):
+    wrapped_abstract_m2m(sender, **kwargs)
 
 @receiver(m2m_changed, sender=Concrete.m2m_field.through)
-def concrete_signal(sender, **kwargs):
-    wrapped_function(sender, **kwargs)
+def concrete_m2m_signal(sender, **kwargs):
+    wrapped_concrete_m2m(sender, **kwargs)
 
-def wrapped_function(sender, **kwargs):
+@receiver(post_save, sender=Abstract)
+def abstract_post_save_signal(sender, **kwargs):
+    wrapped_abstract_post_save(sender, **kwargs)
+
+def wrapped_abstract_m2m(sender, **kwargs):
+    """This function exists to make it easier to mock and count signal calls."""
+    pass
+
+def wrapped_concrete_m2m(sender, **kwargs):
+    """This function exists to make it easier to mock and count signal calls."""
+    pass
+
+def wrapped_abstract_post_save(sender, **kwargs):
+    """This function exists to make it easier to mock and count signal calls."""
     pass
--- app/tests.py
+++ app/tests.py
@@ -1,16 +1,34 @@
 from unittest.mock import patch
 from django.test import TestCase
-from app.models import Other, OtherRelated
+from app.models import Concrete, Other, OtherRelated
 
 def mock_signal(sender, **kwargs):
-    print(sender)
-    print('mocked')
+    pass
 
 class MyTest(TestCase):
 
-    @patch('app.models.wrapped_function', side_effect=mock_signal)
-    def test_example(self, signal_mock):
+    @patch('app.models.wrapped_abstract_m2m', side_effect=mock_signal)
+    def test_abstract_m2m_signal_not_called(self, signal_mock):
         other = Other.objects.create()
         related = OtherRelated.objects.create()
         other.m2m_field.add(related)
         self.assertEqual(signal_mock.call_count, 0)
+
+    @patch('app.models.wrapped_concrete_m2m', side_effect=mock_signal)
+    def test_concrete_m2m_signal_not_called(self, signal_mock):
+        other = Other.objects.create()
+        related = OtherRelated.objects.create()
+        other.m2m_field.add(related)
+        self.assertEqual(signal_mock.call_count, 0)
+
+    @patch('app.models.wrapped_abstract_post_save', side_effect=mock_signal)
+    def test_abstract_post_save_not_called(self, signal_mock):
+        other = Other()
+        other.save()
+        self.assertEqual(signal_mock.call_count, 0)
+
+    @patch('app.models.wrapped_abstract_post_save', side_effect=mock_signal)
+    def test_abstract_post_save_called(self, signal_mock):
+        concrete = Concrete()
+        concrete.save()
+        self.assertNotEqual(signal_mock.call_count, 0)
